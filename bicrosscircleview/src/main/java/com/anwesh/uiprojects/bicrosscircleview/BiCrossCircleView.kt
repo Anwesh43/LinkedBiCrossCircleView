1c339a01532c4cd9a77a77662a5a192f30dbd123
Anwesh43 <anweshthecool0@gmail.com>
2020-09-08 01:18:31 +0530

    adding BiCrossCircleView clas

--- bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
+++ bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBCCNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBiCrossCircle(scale, w, h, paint)
 }
+
+class BiCrossCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
