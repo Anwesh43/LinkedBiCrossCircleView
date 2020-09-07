2504ca68dd6abc64c50af06682262e8952d75911
Anwesh43 <anweshthecool0@gmail.com>
2020-09-08 01:33:24 +0530

    adding BCCNode class

--- bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
+++ bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
@@ -130,4 +130,45 @@ class BiCrossCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCCNode(var i : Int, val state : State = State()) {
+
+        private var next : BCCNode? = null
+        private var prev : BCCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCCNode {
+            var curr : BCCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
