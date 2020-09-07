ea5d86a1989020e97b1df18c401f5411d16295d7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-08 01:17:07 +0530

    adding Canvas extension methods

--- bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
+++ bicrosscircleview/src/main/java/com/anwesh/uiprojects/bicrosscircleview/BiCrossCircleView.kt
@@ -31,3 +31,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiCrossCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / sizeFactor
+    val x : Float = (w / 2 - r)
+    val lineSize : Float = r * lineFactor * sf3
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawCircle(x * sf2, 0f, r * sf1, paint)
+        for (i in 0..1) {
+            save()
+            rotate(45f * (1f - 2 * i) * sf4)
+            paint.color = backColor
+            drawLine(0f, -lineSize, 0f, lineSize, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBCCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = colors[i]
+    drawBiCrossCircle(scale, w, h, paint)
+}
